be1ef8820b9c1ec39ddec33a29d04aba975ecd83
Daniel Chang <daniel.chang85@gmail.com>
2015-12-21 00:52:41 -0500

    PENDING removed human-creator component. setting up route and save-handling for human

--- app/routes/human.js
+++ app/routes/human.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.createRecord('human', {gameId: params.game_id})
+  },
+
+  actions: {
+    createPlayer() {
+      this.model
+        .save()
+        .then((human) => {
+          // this.transitionToRoute('board', human)
+        })
+    }
   }
 });
