a4066b0c02561f5c10de670b322e6a2d5a02c8ba
Daniel Chang <daniel.chang85@gmail.com>
2015-12-27 23:23:38 -0500

    BAD PRACTICE - find ship on didInsertElement

--- app/components/board-cell.js
+++ app/components/board-cell.js
@@ -4,14 +4,19 @@ export default Ember.Component.extend({
   tagName: 'td',
   classNames: 'board-cell',
 
-  classNameBindings: ['hasShip'],
+  classNameBindings: ['ship:has-ship'],
 
   click() {
-    if (this.get('hasShip')) {
+    if (this.get('ship')) {
       alert('Already have a ship on the selected spot')
     } else {
       this.sendAction('addShip', this.row, this.column, this)
-      this.toggleProperty('hasShip');
+      debugger;
+      // this.toggleProperty('hasShip');
     }
+  },
+
+  didInsertElement() {
+    this.set('ship', this.get('board.ships').find(function(ship){return ship.get('row') == this.row && ship.get('column') == this.column}.bind(this)))
   }
 });
