47cbe17cb630460c0e9b9626aabfe00fdaeace8a
Daniel Chang <daniel.chang85@gmail.com>
2016-01-02 23:48:50 -0500

    Preload all data

--- app/models/game.js
+++ app/models/game.js
@@ -2,7 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   winner_id: DS.attr(),
-  // players: DS.hasMany('players', {async: true, polymorphic: true})
   human: DS.belongsTo('human', {async: true}),
-  bot: DS.belongsTo('bot', {async: true})
+  bot: DS.belongsTo('bot', {async: true}),
+  battleshipOn() {
+    // NOT TESTED
+    return (this.winner_id === null || this.winner_id === undefined) && this.get('human.board.hasEnoughShips')
+  }.property('winner_id, human.board.ships.length')
 });
--- app/routes/game.js
+++ app/routes/game.js
@@ -2,15 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   afterModel(model, transition) {
-    // let human = model.get('human').then((human) => {
-          // debugger
-    //   this.store.find('board', human.get('board_id')).then((board) => {
-    //       debugger
-    //     if (!model.battleshipOn()) {
-    //       debugger
-    //       // this.transitionTo('game.new_human', model);
-    //     }
-    //   })
-    // })
+    let promises = [
+      model.get('human'),
+      model.get('bot'),
+      model.get('human').get('board'),
+      model.get('bot').get('board')
+    ]
+
+    return Ember.RSVP.all(promises);
+  },
+
+  setupController(controller, model) {
+    controller.set('human', model.get('human'))
+    controller.set('humanBoard', model.get('human.board'))
+    controller.set('bot', model.get('bot'))
+    controller.set('botBoard', model.get('bot.board'))
+    this.store.find('ship', {board_id: model.get('human.board.id')}).then((ships) => {
+      controller.set('humanBoardShips', ships)
+    })
   }
 });
