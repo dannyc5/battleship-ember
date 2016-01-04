2d8a52b66c6d69e62144bdff4201dc77df17805e
Daniel Chang <daniel.chang85@gmail.com>
2016-01-03 23:05:33 -0500

    MUCH better handling of retrieving lists

--- app/routes/game/battleship.js
+++ app/routes/game/battleship.js
@@ -1,24 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // afterModel(model, transition) {
-  //   let promises = [
-  //     this.store.query('ship', {board_id: model.get('human.board.id')}),
-  //     this.store.query('move', {player_id: model.get('human.id')}),
-  //     this.store.query('move', {player_id: model.get('bot.id')})
-  //   ]
-  //   return Ember.RSVP.all(promises);
-  // },
+  afterModel(model, transition) {
+    let promises = [
+      this.store.query('cell', {player_board_id: model.get('humanBoard.id')}),
+      this.store.query('cell', {player_board_id: model.get('botBoard.id')})
+    ]
+    return Ember.RSVP.all(promises);
+  },
 
-  // setupController(controller, model) {
-  //   controller.set('model', model)
-  //   controller.set('human', model.get('human'))
-  //   controller.set('humanBoard', model.get('human.board'))
-  //   controller.set('bot', model.get('bot'))
-  //   controller.set('botBoard', model.get('bot.board'))
-
-  //   controller.set('humanBoardShips', model.get('human.board.ships'))
-  //   controller.set('humanMoves', model.get('human.moves'))
-  //   controller.set('botMoves', model.get('bot.moves'))
-  // }
+  setupController(controller, model) {
+    controller.set('model', model)
+    controller.set('humanBoard', model.get('humanBoard'))
+    controller.set('botBoard', model.get('botBoard'))
+    controller.set('humanBoardCells', model.get('humanBoard.cells'))
+    controller.set('botBoardCells', model.get('botBoard.cells'))
+  }
 });
--- app/routes/game/human-board/cells/new-ships.js
+++ app/routes/game/human-board/cells/new-ships.js
@@ -2,15 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   afterModel(model, transition) {
-    // model is humanBoard
+    // model === game.humanBoard
+    let promises = [
+      this.store.query('cell', {player_board_id: model.get('id')}),
+    ]
+    return Ember.RSVP.all(promises);
 
-    // TODO: run this after ships have loaded
-    if (model.get('hasEnoughShips')) {
-      this.transitionTo('game.battleship', this.modelFor('game'))
-    }
   },
 
   setupController(controller, model) {
+    if (model.get('hasEnoughShips')) {
+      this.transitionTo('game.battleship', this.modelFor('game'))
+    }
     controller.set('model', model.get('cells'))
   }
 });
